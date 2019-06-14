a83c4a1b3bbe1539db5a9e0c219ec147865159c6
flexkat <catherinemaritz1@gmail.com>
2019-06-14 15:05:58 +0100

    stylist CRUD and views made

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,2 +1,41 @@
 class StylistsController < ApplicationController
+
+  def index
+    @stylists = Stylist.all
+  end
+
+  def new
+    @stylist = Stylist.new
+  end
+
+  def create
+    stylist = Stylist.create stylist_params
+    redirect_to stylist
+  end
+
+  def show
+    @stylist = Stylist.find params[:id]
+  end 
+
+  def edit
+    @stylist = Stylist.find params[:id]
+  end 
+
+  def update
+    stylist = Stylist.find params[:id]
+    stylist.update stylist_params
+    redirect_to stylist
+  end
+
+  def destroy
+    stylist = Stylist.find params[:id]
+    stylist.destroy
+    redirect_to stylists_path
+  end 
+
+  private
+
+  def stylist_params
+    params.require(:stylist).permit(:name, :job_title, :salon_id)
+  end
 end
