53561bb1e6c56fcbd14d1e6f3a293abb8e6e828c
flexkat <catherinemaritz1@gmail.com>
2019-06-15 10:58:35 +0100

    treatment CRU and views

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,2 +1,35 @@
 class TreatmentsController < ApplicationController
+
+  def index
+    @treatments = Treatment.all
+  end
+
+  def new
+    @treatment = Treatment.new
+  end
+
+  def create
+    treatment = Treatment.create treatment_params
+    redirect_to treatment
+  end
+
+  def show
+    @treatment = Treatment.find params[:id]
+  end
+
+  def edit
+    @treatment = Treatment.find params[:id]
+  end
+
+  def update
+    treatment = Treatment.update treatment_params
+    redirect_to treatment
+  end
+
+  private
+
+  def treatment_params
+    params.require(:treatment).permit(:name, :description, :price, :time)
+  end
+
 end
