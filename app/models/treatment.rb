c5dcecd34f162d434299d2f823a1a0214d04fa72
flexkat <catherinemaritz1@gmail.com>
2019-06-18 12:07:12 +0100

    added uniq validation to treatment name

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,5 +1,5 @@
 class Treatment < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, numericality: true
   validates :time, presence: true, numericality: true
