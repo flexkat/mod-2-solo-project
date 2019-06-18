4a9f4a87ee44998935e4eefe9751a565edfc1b14
flexkat <catherinemaritz1@gmail.com>
2019-06-18 20:31:45 +0100

    created Sessionscontroller, login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/name'
-  get 'user/email'
-  get 'user/password_digest'
+  
+  get '/signup', to: "users#new", as: "signup_path"
+  get '/login', to: "sessions#new", as: "login_path"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   resources :services
   resources :treatment_categories
   resources :stylist_treatments
@@ -9,5 +11,6 @@ Rails.application.routes.draw do
   resources :stylists
   resources :salons
   resources :locations
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
