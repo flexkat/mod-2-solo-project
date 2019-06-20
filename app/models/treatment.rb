993d73d59b0ff761400fdd1df9e571ed09302015
flexkat <catherinemaritz1@gmail.com>
2019-06-20 22:55:47 +0100

    filter treatment by salon on tx show page

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -4,10 +4,15 @@ class Treatment < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :time, presence: true, numericality: true
 
-
-
-
   has_many :stylist_treatments
   belongs_to :service
 
+  def treatment_salons
+    Salon.all.select{|salon| salon.treatments.include?(self)}
+  end 
+
+  def self.filter_by_service params
+    treatments = params[:service_id] ? Treatment.where(service_id: params[:service_id]) : Treatment.all
+    treatments
+  end
 end
