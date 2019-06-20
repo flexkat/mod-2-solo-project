2e3e3ec1dd7c0487d70dcf05ca6edcbafca51b41
flexkat <catherinemaritz1@gmail.com>
2019-06-20 22:53:41 +0100

    root route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  root to: "salons#index"
   get '/signup', to: "users#new", as: "signup"
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy", as: "logout"
+
   resources :services
   resources :treatment_categories
   resources :stylist_treatments
